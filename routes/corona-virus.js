23985baa26c1bf3c297f76e0e03de49b9695c6d2
awais786327 <awais3227443@gmail.com>
2020-02-29 02:38:06 +0500

    landing page

--- routes/corona-virus.js
+++ routes/corona-virus.js
@@ -30,10 +30,7 @@ const baseUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19`;
 const branch = `master`;
 const directory = `csse_covid_19_data`;
 const dailyReports = `csse_covid_19_daily_reports`;
-const yesterday = moment().subtract(1, 'days').format('MM-DD-YYYY');
-const file = `${yesterday}.csv`;
 
-const url = `${baseUrl}/${branch}/${directory}/${dailyReports}/${file}`;
 const websiteUrl = `https://systems.jhu.edu/research/public-health/ncov/`;
 
 const links = [];
@@ -47,8 +44,16 @@ function getLatestUpdatesUrl() {
   return links;
 }
 
+function getUrl() {
+  const yesterday = moment().subtract(1, 'days').format('MM-DD-YYYY');
+  const file = `${yesterday}.csv`;
+  console.log('file ', file);
+  return `${baseUrl}/${branch}/${directory}/${dailyReports}/${file}`;
+}
+
 function getLatestUpdates() {
-  updateUrl();
+  const url = getUrl();
+  updateUrl(url);
   return axios.get(url)
     .then(function (response) {
       const { data } = response;
@@ -60,7 +65,7 @@ function getLatestUpdates() {
     });
 }
 
-function updateUrl() {
+function updateUrl(url) {
   const url1 = bitly.shorten(url);
   const url2 = bitly.shorten(websiteUrl);
   Promise.all([url1, url2])
