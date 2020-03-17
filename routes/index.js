d5106a75434802f189b051a94bb4a4527840c40f
awais786327 <awais3227443@gmail.com>
2020-03-17 14:40:36 +0500

    logs

--- routes/index.js
+++ routes/index.js
@@ -145,6 +145,7 @@ router.post('/webhook', (req, res, next) => {
 
   function urlShortener(agent) {
     const longUrl = agent.parameters.url;
+    console.log(`longUrl >`, longUrl);
     return bitly
       .shorten(longUrl)
       .then(function(result) {
