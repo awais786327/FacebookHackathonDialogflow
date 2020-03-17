b1077576f7d4af0d505adb7ffdfac98ec533286e
awais786327 <awais3227443@gmail.com>
2020-03-17 15:00:30 +0500

    logs

--- routes/index.js
+++ routes/index.js
@@ -143,23 +143,26 @@ router.post('/webhook', (req, res, next) => {
     }, reminderTime);
   }
 
-  function urlShortener(agent) {
-    const longUrl = agent.parameters.url;
+  function shortUrl(agent) {
     console.log(`longUrl > `, agent.parameters);
-    /*return bitly
-      .shorten(longUrl)
-      .then(function(result) {
-        console.log(`\n`);
-        console.log('result ', result);
-        console.log(`\n`);
-        agent.add(`there you go`);
-        return agent.add(result.url);
-      })
-      .catch(function(error) {
-        console.log(error);
-        agent.add(`This is not a valid URL`);
-        return agent.add(`URL must start with https:// followed by URL\ne.g: https://www.google.com`);
-      });*/
+    return agent.add(`ok short url`);
+    /*
+        const longUrl = agent.parameters.url;
+        return bitly
+          .shorten(longUrl)
+          .then(function(result) {
+            console.log(`\n`);
+            console.log('result ', result);
+            console.log(`\n`);
+            agent.add(`there you go`);
+            return agent.add(result.url);
+          })
+          .catch(function(error) {
+            console.log(error);
+            agent.add(`This is not a valid URL`);
+            return agent.add(`URL must start with https:// followed by URL\ne.g: https://www.google.com`);
+          });
+    */
   }
 
   function computerHacks(agent) {
@@ -411,7 +414,7 @@ router.post('/webhook', (req, res, next) => {
   intentMap.set('Find Phone', findPhone);
   intentMap.set('Create Event - write', createEvent);
   intentMap.set('Reminder', reminder);
-  intentMap.set('Url Shortener', urlShortener);
+  intentMap.set('Url Shortener', shortUrl);
   intentMap.set('Computer Hacks - options', computerHacks);
   intentMap.set('Computer Hacks - Play Again - yes', computerHacksPlayAgain);
   intentMap.set('Slack Announcement - write', slackAnnouncement);
